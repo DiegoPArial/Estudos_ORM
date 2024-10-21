562018d22fd2920ad0547bc2fd5eeaf966962b94
Diego Preissler Arial <diprearial@gmail.com>
2024-10-21 11:28:40 -0300

    Estudos ORM

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: 'mysql',
+    host: 'localhost',
+    username: 'root',
+    password: 'Diego020304.',
+    database: 'sqlnode',
+    define: {
+        timestamps: true,
+        underscored : true,
+    },
+};
